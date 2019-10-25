a8155bd3095b731340e7a44264d1842deed7986b
Tshepo Julius Mboweni <Tshepo.Mboweni@eoh.com>
2019-10-25 12:45:17 +0200

    footer in progress

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { MenubarComponent } from './menubar/menubar.component';
+import { MenubarComponent } from "./menubar/menubar.component";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MenubarComponent],
+  declarations: [AppComponent, HomeComponent, MenubarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
--- Portfolio/src/app/menubar/menubar.component.ts
+++ Portfolio/src/app/menubar/menubar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import * as $ from "jquery";
+// import * as $ from "jquery";
 
 @Component({
   selector: "app-menubar",
@@ -10,24 +10,46 @@ export class MenubarComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    $(document).ready(() => {
-      $(window).scroll(() => {
-        const scroll = $(window).scrollTop();
-        if (scroll > 100) {
-          $(".navbar").css({
-            background: "#fff",
-            "transition-duration": "4s"
-          });
-
-          $(".nav-link").css({
-            color: "#000",
-            "transition-duration": "1s"
-          });
-        } else {
-          $(".navbar").css("background", "transparent");
-          $(".nav-link").css("color", "#fff");
-        }
-      });
-    });
+    // $(document).ready(() => {
+    //   // change navbar color on scroll
+    //   $(window).scroll(() => {
+    //     const scroll = $(window).scrollTop();
+    //     if (scroll > 100) {
+    //       $(".navbar").css({
+    //         background: "#fff",
+    //         "transition-duration": "4s"
+    //       });
+    //       $(".nav-link").css({
+    //         color: "#000",
+    //         "transition-duration": "1s"
+    //       });
+    //     } else {
+    //       $(".navbar").css("background", "transparent");
+    //       $(".nav-link").css("color", "#fff");
+    //     }
+    //   });
+    //   // Add smooth scrolling to all links
+    //   $("a").on("click", function(event) {
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "50") {
+    //       // Prevent default anchor click behavior
+    //       event.preventDefault();
+    //       // Store hash
+    //       const hash = this.hash;
+    //       // Using jQuery's animate() method to add smooth page scroll
+    //       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //       $("html, body").animate(
+    //         {
+    //           scrollTop: $(hash).offset().top
+    //         },
+    //         500,
+    //         function() {
+    //           // Add hash (#) to URL when done scrolling (default click behavior)
+    //           window.location.hash = hash;
+    //         }
+    //       );
+    //     } // End if
+    //   });
+    // });
   }
 }
