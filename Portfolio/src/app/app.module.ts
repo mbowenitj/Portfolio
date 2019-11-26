482e875627e48e894d21afa85285c36ce3000820
Tshepo Julius Mboweni <Tshepo.Mboweni@eoh.com>
2019-11-26 18:44:10 +0200

    form in progress

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from "@angular/platform-browser";
 // import { AlertModule } from "ngx-bootstrap";
 import { NgModule } from "@angular/core";
-
+import { MatSelectModule, MatInputModule } from "@angular/material";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { MenubarComponent } from "./menubar/menubar.component";
-import { FooterComponent } from './footer/footer.component';
-import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from "./footer/footer.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+// import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MenubarComponent, FooterComponent, SkillsComponent, PortfolioComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenubarComponent,
+    FooterComponent,
+    SkillsComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Portfolio/src/app/home/home.component.ts
+++ Portfolio/src/app/home/home.component.ts
@@ -11,6 +11,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function() {
+      // $(window).scroll(function() {
+      //   if ($(this).scrollTop() === 100) {
+      //     // this refers to window
+      //     alert("You've scrolled 100 pixels.");
+      //   }
+      // });
       // run the fade() function every 2 seconds
       setInterval(function() {
         fade();
@@ -20,6 +26,49 @@ export class HomeComponent implements OnInit {
       function fade() {
         $(".jobRole").fadeToggle(500);
       }
+
+      // $(window).scroll(function() {
+      $(window).scroll(testScroll);
+
+      var viewed = false;
+
+      function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return elemBottom <= docViewBottom && elemTop >= docViewTop;
+      }
+
+      function testScroll() {
+        if (isScrolledIntoView($("#counter")) && !viewed) {
+          viewed = true;
+          $(".Count").each(function() {
+            $(this)
+              .prop("Counter", 0)
+              .animate(
+                {
+                  Counter: $(this).text()
+                },
+                {
+                  duration: 4000,
+                  easing: "swing",
+                  step: function(now) {
+                    $(this).text(Math.ceil(now));
+                  }
+                }
+              );
+          });
+        }
+      }
+      // function testUp() {
+      //   if (isScrolledIntoView($("#portfolio")) && !viewed) {
+      //     viewed = true;
+      //     alert("ninja");
+      //   }
+      // }
     });
   }
 }
